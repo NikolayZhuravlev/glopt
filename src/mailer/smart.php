ed308fd3a203e6ffd69135043227dcec3f597177
Nik <zhuravlev_nikola@mail.ru>
2021-02-28 18:47:06 +0500

    max-width: 575px

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -16,7 +16,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'nikglopt@mail.ru';                 // Наш логин
-$mail->Password = 'priora262';                           // Наш пароль от ящика
+$mail->Password = 'glopt2021';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = '465';                                    // TCP port to connect to
  
